9c3cd6ef6d90ae40745c4fb874e109bf7f4b800c
dima18127 <98730001+dima18127@users.noreply.github.com>
2023-06-17 00:28:08 +0400

    Updates

---
+++ precache-manifest.69a823309850d8cff3c343dfde60b91c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "2ba2b0bcd75556d8aba0",
+    "url": "/react-anketa/static/css/main.40ba74e3.chunk.css"
+  },
+  {
+    "revision": "2ba2b0bcd75556d8aba0",
+    "url": "/react-anketa/static/js/main.2ba2b0bc.chunk.js"
+  },
+  {
+    "revision": "2e2510b3efffeabcf261",
+    "url": "/react-anketa/static/js/1.2e2510b3.chunk.js"
+  },
+  {
+    "revision": "3b43632f2a885fc9c3e5",
+    "url": "/react-anketa/static/js/runtime~main.3b43632f.js"
+  },
+  {
+    "revision": "d996a56bd9424b59056721e12081c457",
+    "url": "/react-anketa/static/media/Folder.d996a56b.svg"
+  },
+  {
+    "revision": "8c4b895d09ac7355fd1ca5cf83fdece0",
+    "url": "/react-anketa/index.html"
+  }
+];
